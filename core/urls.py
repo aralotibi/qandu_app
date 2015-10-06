e87b608b947561526271ed6c27964509bc159a5c
Abdullah <aralotibi@quinnipiac.edu>
2015-10-06 15:28:18 +0000

    Add answer update view

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
-  url(r'^user/', include('registration.backends.simple.urls')),
-  url(r'^user/', include('django.contrib.auth.urls')),
+    url(r'^user/', include('registration.backends.simple.urls')),
+    url(r'^user/', include('django.contrib.auth.urls')),
                        url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
     url(r'question/$', QuestionListView.as_view(), name='question_list'),
 
@@ -17,6 +17,7 @@ url(r'^question/update/(?P<pk>\d+)/$', QuestionUpdateView.as_view(), name='quest
 
      url(r'^question/(?P<pk>\d+)/answer/create/$', AnswerCreateView.as_view(), name='answer_create'),
 
+  url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', AnswerUpdateView.as_view(), name='answer_update'),
 
 
 
